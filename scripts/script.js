b7cc2e395fe77f251ff5c05d0f456608e5e5b07c
singhsanjiv200725-ux <singhsanjiv.200725@gmail.com>
2025-08-19 02:05:49 +0530

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,162 @@
+//// Wardrobe sets with all images
+const wardrobes = {
+  dress1: [
+    "Images/image1/image1_1.jpg", "Images/image1/image1_2.jpg", "Images/image1/image1_3.jpg",
+    "Images/image1/image1_4.jpg", "Images/image1/image1_5.jpg", "Images/image1/image1_6.jpg",
+    "Images/image1/image1_7.jpg", "Images/image1/image1_8.jpg", "Images/image1/image1_9.jpg",
+    "Images/image1/image1_10.jpg", "Images/image1/image1_11.jpg", "Images/image1/image1_12.jpg",
+    "Images/image1/image1_13.jpg", "Images/image1/image1_14.jpg", "Images/image1/image1_15.jpg",
+    "Images/image1/image1_16.jpg", "Images/image1/image1_17.jpg", "Images/image1/image1_18.jpg",
+    "Images/image1/image1_19.jpg", "Images/image1/image1_20.jpg", "Images/image1/image1_21.jpg",
+    "Images/image1/image1_22.jpg", "Images/image1/image1_23.jpg", "Images/image1/image1_24.jpg",
+    "Images/image1/image1_25.jpg", "Images/image1/image1_26.jpg", "Images/image1/image1_27.jpg",
+    "Images/image1/image1_28.jpg", "Images/image1/image1_29.jpg", "Images/image1/image1_30.jpg",
+    "Images/image1/image1_31.jpg", "Images/image1/image1_32.jpg", "Images/image1/image1_33.jpg",
+    "Images/image1/image1_34.jpg", "Images/image1/image1_35.jpg", "Images/image1/image1_36.jpg",
+    "Images/image1/image1_37.jpg", "Images/image1/image1_38.jpg", "Images/image1/image1_39.jpg",
+    "Images/image1/image1_40.jpg", "Images/image1/image1_41.jpg", "Images/image1/image1_42.jpg"
+  ],
+  dress2: [
+    "Images/image2/image2_1.jpg", "Images/image2/image2_2.jpg", "Images/image2/image2_3.jpg",
+    "Images/image2/image2_4.jpg", "Images/image2/image2_5.jpg", "Images/image2/image2_6.jpg",
+    "Images/image2/image2_7.jpg", "Images/image2/image2_8.jpg", "Images/image2/image2_9.jpg",
+    "Images/image2/image2_10.jpg", "Images/image2/image2_11.jpg", "Images/image2/image2_12.jpg",
+    "Images/image2/image2_13.jpg", "Images/image2/image2_14.jpg", "Images/image2/image2_15.jpg",
+    "Images/image2/image2_16.jpg", "Images/image2/image2_17.jpg", "Images/image2/image2_18.jpg",
+    "Images/image2/image2_19.jpg", "Images/image2/image2_20.jpg", "Images/image2/image2_21.jpg",
+    "Images/image2/image2_22.jpg", "Images/image2/image2_23.jpg", "Images/image2/image2_24.jpg",
+    "Images/image2/image2_25.jpg", "Images/image2/image2_26.jpg", "Images/image2/image2_27.jpg",
+    "Images/image2/image2_28.jpg", "Images/image2/image2_29.jpg", "Images/image2/image2_30.jpg",
+    "Images/image2/image2_31.jpg", "Images/image2/image2_32.jpg", "Images/image2/image2_33.jpg",
+    "Images/image2/image2_34.jpg"
+  ],
+  dress3: [
+    "Images/image3/image3_1.jpg", "Images/image3/image3_2.jpg", "Images/image3/image3_3.jpg",
+    "Images/image3/image3_4.jpg", "Images/image3/image3_5.jpg", "Images/image3/image3_6.jpg",
+    "Images/image3/image3_7.jpg", "Images/image3/image3_8.jpg", "Images/image3/image3_9.jpg",
+    "Images/image3/image3_10.jpg", "Images/image3/image3_11.jpg", "Images/image3/image3_12.jpg",
+    "Images/image3/image3_13.jpg", "Images/image3/image3_14.jpg", "Images/image3/image3_15.jpg",
+    "Images/image3/image3_16.jpg", "Images/image3/image3_17.jpg", "Images/image3/image3_18.jpg",
+    "Images/image3/image3_19.jpg", "Images/image3/image3_20.jpg", "Images/image3/image3_21.jpg",
+    "Images/image3/image3_22.jpg", "Images/image3/image3_23.jpg", "Images/image3/image3_24.jpg",
+    "Images/image3/image3_25.jpg", "Images/image3/image3_26.jpg", "Images/image3/image3_27.jpg",
+    "Images/image3/image3_28.jpg", "Images/image3/image3_29.jpg", "Images/image3/image3_30.jpg",
+    "Images/image3/image3_31.jpg", "Images/image3/image3_32.jpg", "Images/image3/image3_33.jpg",
+    "Images/image3/image3_34.jpg", "Images/image3/image3_35.jpg", "Images/image3/image3_36.jpg",
+    "Images/image3/image3_37.jpg", "Images/image3/image3_38.jpg", "Images/image3/image3_39.jpg",
+    "Images/image3/image3_40.jpg", "Images/image3/image3_41.jpg", "Images/image3/image3_42.jpg",
+    "Images/image3/image3_43.jpg", "Images/image3/image3_44.jpg"
+  ],
+  dress4: [
+    "Images/image4/image4_1.jpg", "Images/image4/image4_2.jpg", "Images/image4/image4_3.jpg",
+    "Images/image4/image4_4.jpg", "Images/image4/image4_5.jpg", "Images/image4/image4_6.jpg",
+    "Images/image4/image4_7.jpg", "Images/image4/image4_8.jpg", "Images/image4/image4_9.jpg",
+    "Images/image4/image4_10.jpg", "Images/image4/image4_11.jpg", "Images/image4/image4_12.jpg",
+    "Images/image4/image4_13.jpg","Images/image4/image4_14.jpg","Images/image4/image4_15.jpg"
+  ],
+  dress5: [
+    "Images/image5/image5_1.jpg", "Images/image5/image5_2.jpg", "Images/image5/image5_3.jpg",
+    "Images/image5/image5_4.jpg", "Images/image5/image5_5.jpg", "Images/image5/image5_6.jpg",
+    "Images/image5/image5_7.jpg", "Images/image5/image5_8.jpg", "Images/image5/image5_9.jpg",
+    "Images/image5/image5_10.jpg", "Images/image5/image5_11.jpg", "Images/image5/image5_12.jpg",
+    "Images/image5/image5_13.jpg", "Images/image5/image5_14.jpg", "Images/image5/image5_15.jpg",
+    "Images/image5/image5_16.jpg", "Images/image5/image5_17.jpg", "Images/image5/image5_18.jpg",
+    "Images/image5/image5_19.jpg", "Images/image5/image5_20.jpg", "Images/image5/image5_21.jpg",
+    "Images/image5/image5_22.jpg", "Images/image5/image5_23.jpg", "Images/image5/image5_24.jpg",
+    "Images/image5/image5_25.jpg", "Images/image5/image5_26.jpg", "Images/image5/image5_27.jpg",
+    "Images/image5/image5_28.jpg"
+  ],
+  dress6: [
+    "Images/image6/image6_1.jpg", "Images/image6/image6_2.jpg", "Images/image6/image6_3.jpg",
+    "Images/image6/image6_4.jpg", "Images/image6/image6_5.jpg", "Images/image6/image6_6.jpg",
+    "Images/image6/image6_7.jpg", "Images/image6/image6_8.jpg", "Images/image6/image6_9.jpg",
+    "Images/image6/image6_10.jpg", "Images/image6/image6_11.jpg", "Images/image6/image6_12.jpg",
+    "Images/image6/image6_13.jpg", "Images/image6/image6_14.jpg", "Images/image6/image6_15.jpg",
+    "Images/image6/image6_16.jpg", "Images/image6/image6_17.jpg"
+  ]
+};
+
+
+// 🎵 Music tracks for each dress
+const musicTracks = {
+  dress1: "music/dress1.mp3",
+  dress2: "music/dress2.mp3",
+  dress3: "music/dress3.mp3",
+  dress4: "music/dress4.mp3",
+  dress5: "music/dress5.mp3",
+  dress6: "music/dress6.mp3"
+};
+
+let currentSet = [];
+let index = 0;
+let slideInterval;
+let audio = null;
+let isMuted = false;
+
+function openModal(setName) {
+  currentSet = wardrobes[setName];
+  index = 0;
+  document.getElementById("myModal").style.display = "block";
+  document.getElementById("modalImg").src = currentSet[index];
+
+  document.body.classList.add("blurred");
+
+  slideInterval = setInterval(() => {
+    changeSlide(1);
+  }, 1000);
+
+  playMusic(setName);
+}
+
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
+  document.body.classList.remove("blurred");
+  clearInterval(slideInterval);
+
+  stopMusic();
+}
+
+function changeSlide(n) {
+  if (currentSet.length === 0) return;
+  index = (index + n + currentSet.length) % currentSet.length;
+  document.getElementById("modalImg").src = currentSet[index];
+}
+
+// 🎶 Handle music
+function playMusic(setName) {
+  stopMusic();
+  if (musicTracks[setName]) {
+    audio = new Audio(musicTracks[setName]);
+    audio.loop = true;
+    audio.play();
+    isMuted = false;
+    document.getElementById("musicToggle").textContent = "💓";
+  }
+}
+
+function stopMusic() {
+  if (audio) {
+    audio.pause();
+    audio.currentTime = 0;
+    audio = null;
+  }
+}
+
+// 💓 toggle mute/unmute
+document.getElementById("musicToggle").addEventListener("click", () => {
+  if (!audio) return;
+  isMuted = !isMuted;
+  audio.muted = isMuted;
+  document.getElementById("musicToggle").textContent = isMuted ? "💔" : "💓";
+});
+
+// 📸 attach grid clicks
+document.querySelectorAll("#grid a").forEach(link => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    let group = link.getAttribute("data-group");
+    if (wardrobes[group]) {
+      openModal(group);
+    }
+  });
+});
